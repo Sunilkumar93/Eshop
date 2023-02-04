0486b1afb0ae09d708dbf893fe6dcbd231de0af8
Sunilkumar93 <sunilkumarsihag098@gmail.com>
2023-02-05 00:31:41 +0530

    product update and delete routes added

--- Backend/Controlers/product.controler.js
+++ Backend/Controlers/product.controler.js
@@ -1,5 +1,6 @@
 const { catchAsyncError } = require("../middleware/catchAsyncError.middleware");
 const { ProductModel } = require("../Models/Product.model.js");
+const { ErrorHandler } = require("../utils/ErrorHandler");
 // Create A Product
 
 const createProduct = catchAsyncError(async (req, res, next) => {
@@ -14,9 +15,47 @@ const createProduct = catchAsyncError(async (req, res, next) => {
   });
 });
 
+// Get All Product
 const getAllProducts = catchAsyncError(async (req, res, next) => {
   const products = await ProductModel.find();
   res.status(200).send({ error: false, data: products });
 });
 
-module.exports = { getAllProducts, createProduct };
+// Update Product By Id
+
+const updateProduct = catchAsyncError(async (req, res, next) => {
+  const id = req.params.id;
+
+  let product = await ProductModel.findById(id);
+
+  if (!product) {
+    return next(new ErrorHandler(`Product Is Not Found By Id:${id}`, 404));
+  }
+  product = await ProductModel.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(201).send({ error: false, data: product });
+});
+
+// Delete Product By Id
+
+const deleteProduct = catchAsyncError(async (req, res, next) => {
+  const id = req.params.id;
+
+  let product = await ProductModel.findById(id);
+
+  if (!product) {
+    return next(new ErrorHandler(`Product Is Not Found By Id:${id}`, 404));
+  }
+  product = await ProductModel.findByIdAndDelete(id);
+  res
+    .status(201)
+    .send({
+      error: false,
+      message: `Product With id:${id} Deleted SuccessFully`,
+    });
+});
+
+module.exports = { getAllProducts, createProduct, updateProduct,deleteProduct };
